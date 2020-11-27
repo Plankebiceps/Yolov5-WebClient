49728a071bdbba2230621f7b6cd62bff6a1766ed
Victor <viggo10001@hotmail.com>
2020-11-27 11:18:18 +0100

    Lavet et interface, tilføjet Client_Secret, Slettet Sites master, Installeret en masse packages

---
+++ WebClient Commentor/App_Start/IServiceCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WebClient_Commentor.App_Start
+{
+    public interface IServiceCollection :
+        System.Collections.Generic.ICollection<Microsoft.Extensions.DependencyInjection.ServiceDescriptor>,
+        System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor>,
+        System.Collections.Generic.IList<Microsoft.Extensions.DependencyInjection.ServiceDescriptor>
+    {
+        object AddAuthentication(Action<object> p);
+        void AddRazorPages();
+        void AddDbContext<T>(Func<object, object> p);
+
+        public string DefaultAuthenticateScheme { get; set; }
+    }
+
+}
